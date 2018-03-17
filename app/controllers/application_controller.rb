fe9b99b3476d41d4bd1e58cf7bcec5633a87fbd5
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-16 23:14:23 -0400

    finish application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    @current_user = user
+    session[:token] = user.reset_session_token!
+  end
+
+  def logout
+    current_user.try(reset_session_token!)
+    session[:token] = nil
+  end
+
+  def logged_in?
+    !!@current_user
+  end
+
+  def logged_out?
+    @current_user.nil?
+  end
 end
