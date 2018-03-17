7a1851c0dc20110e7fd4fe44ae36a7f2485c5af3
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-16 20:37:40 -0400

    pushing auto generated user test controller

--- test/models/sub_test.rb
+++ test/models/sub_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id           :integer          not null, primary key
+#  title        :string           not null
+#  url          :string
+#  content      :string
+#  moderator_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class SubTest < ActiveSupport::TestCase
