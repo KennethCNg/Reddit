248d4357779c80463d81a0a0fefbebf56d2603be
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-17 18:55:12 -0400

    add route files

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :subs, only: [:index, :show, :new, :create, :update, :edit, :destroy]
+  resources :posts, only: [:new, :create, :show, :destroy, :update, :edit]
 end
