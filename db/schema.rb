39e0f3e8ddc8e45545b31d94e836e6e846ef6a95
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-18 03:52:33 -0400

    add comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318010657) do
+ActiveRecord::Schema.define(version: 20180318073115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "author_id", null: false
+    t.integer "post_id", null: false
+    t.integer "parent_comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parent_comment_id"], name: "index_comments_on_parent_comment_id"
+  end
 
   create_table "post_subs", force: :cascade do |t|
     t.integer "post_id"
